3297163c9182362b2c0beaa1ffb5bff87149e68f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-09 18:14:25 +0100

    Task completed

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+import "./style.css";
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, now imported
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
